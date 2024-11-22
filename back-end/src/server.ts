1721d1cc951c564a1fe009307da3657c1a620dbe
ALBAAPEREZ <alu0101513768@ull.edu.es>
2024-11-22 09:42:45 +0000

    cambios

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -8,7 +8,7 @@ import productRoutes from './routes/productRoutes.js'; // Rutas de producto
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8080;
 
 const startServer = async () => {
   try {
